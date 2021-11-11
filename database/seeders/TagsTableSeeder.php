8ed1569ce0d98f517bd53feb8b4c83dcf2b0ca89
中村　迅 <shu.kyu0224@icloud.com>
2021-11-12 07:02:13 +0900

    seed追加

--- database/seeders/TagsTableSeeder.php
+++ database/seeders/TagsTableSeeder.php
@@ -21,20 +21,30 @@ class TagsTableSeeder extends Seeder
         ];
         $item = new Tag;
         $item->fill($param)->save();
+
         $param = [
             'id' => 2,
             'tag_name' => '揚げ物'
         ];
         $item = new Tag;
         $item->fill($param)->save();
+
         $param = [
             'id' => 3,
             'tag_name' => '野菜料理'
         ];
         $item = new Tag;
         $item->fill($param)->save();
+
         $param = [
             'id' => 4,
+            'tag_name' => '定番おつまみ'
+        ];
+        $item = new Tag;
+        $item->fill($param)->save();
+
+        $param = [
+            'id' => 5,
             'tag_name' => 'ごはんもの'
         ];
         $item = new Tag;
